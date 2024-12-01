0dc2191635ca827c736f9e8151fc08d6ce2c3d84
Mattias Hermansson <hexagenic@gmail.com>
2024-12-01 16:20:55 +0100

    reformat with prettier

--- 2024/src/day01.ts
+++ 2024/src/day01.ts
@@ -1,51 +1,51 @@
-import fs from "fs";
+import fs from 'fs';
 import readline from 'readline';
 
 export async function day01_part1(): Promise<number> {
-    const input = fs.createReadStream('input/day01.txt');
-    const rl = readline.createInterface({ input });
+  const input = fs.createReadStream('input/day01.txt');
+  const rl = readline.createInterface({ input });
 
-    const ls: number[] = [];
-    const rs: number[] = [];
+  const ls: number[] = [];
+  const rs: number[] = [];
 
-    for await (const line of rl) {
-        const [l, r] = line.split('   ');
-        ls.push(parseInt(l));
-        rs.push(parseInt(r))
-    }
+  for await (const line of rl) {
+    const [l, r] = line.split('   ');
+    ls.push(parseInt(l));
+    rs.push(parseInt(r));
+  }
 
-    ls.sort((a, b) => a - b);
-    rs.sort((a, b) => a - b);
+  ls.sort((a, b) => a - b);
+  rs.sort((a, b) => a - b);
 
-    let sum = 0;
-    for (let i = 0; i < ls.length; i++) {
-        sum += Math.abs(ls[i] - rs[i]);
-    }
+  let sum = 0;
+  for (let i = 0; i < ls.length; i++) {
+    sum += Math.abs(ls[i] - rs[i]);
+  }
 
-    return sum;
+  return sum;
 }
 
 export async function day01_part2(): Promise<number> {
-    const input = fs.createReadStream('input/day01.txt');
-    const rl = readline.createInterface({ input });
+  const input = fs.createReadStream('input/day01.txt');
+  const rl = readline.createInterface({ input });
 
-    const ls: number[] = [];
-    const rs: number[] = [];
+  const ls: number[] = [];
+  const rs: number[] = [];
 
-    for await (const line of rl) {
-        const [l, r] = line.split('   ');
-        ls.push(parseInt(l));
-        rs.push(parseInt(r))
-    }
+  for await (const line of rl) {
+    const [l, r] = line.split('   ');
+    ls.push(parseInt(l));
+    rs.push(parseInt(r));
+  }
 
-    const lSet = new Set(ls);
+  const lSet = new Set(ls);
 
-    let sum = 0;
+  let sum = 0;
 
-    for (const l of lSet.values()) {
-        const count = rs.filter(r => r === l).length;
-        sum += count * l;
-    }
+  for (const l of lSet.values()) {
+    const count = rs.filter((r) => r === l).length;
+    sum += count * l;
+  }
 
-    return sum;
+  return sum;
 }
--- 2024/src/index.ts
+++ 2024/src/index.ts
@@ -1,8 +1,28 @@
-import { day01_part1, day01_part2 } from "./day01";
+import { day01_part1, day01_part2 } from './day01';
+
+type Solution = () => Promise<number>;
+
+async function print_solution(day: number, part1: Solution, part2?: Solution) {
+  return {
+    day,
+    part1: await part1(),
+    part2: await part2?.(),
+  };
+}
 
 async function main() {
-    console.log('01 a:', await day01_part1());
-    console.log('01 b:', await day01_part2());
+  const solutions = await Promise.all([
+    print_solution(1, day01_part1, day01_part2),
+  ]);
+
+  const solutionByDay = solutions.reduce<
+    Record<number, { part1: number; part2?: number }>
+  >((acc, { day, ...parts }) => {
+    acc[day] = parts;
+    return acc;
+  }, {});
+
+  console.table(solutionByDay);
 }
 
 main();
